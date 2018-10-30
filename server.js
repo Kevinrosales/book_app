a387e54d414dbf7f2560beebc5c3a62ce15a9b99
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-10-30 10:31:03 -0700

    added landing page with a form

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ require(`dotenv`).config();
 
 let app = express();
 app.set('view engine','ejs');
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`app is up on ${PORT}`));
+app.get('/', (req, res) => {
+    res.render('pages/index');
+})
